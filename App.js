a7b0f3a039d1226bb87fd0c0baf289a11e1bb2dd
Engin Yenice <enginyenice2626@gmail.com>
2021-05-31 02:24:16 +0300

    BeyouthifulMedia

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Home></Home>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "row"
   },
 });
